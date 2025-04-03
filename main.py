32741b926340550be0eab904b9fbc9839ba15934
TheSlumberingEgg <aramawad05@gmail.com>
2025-04-03 18:55:35 -0400

    Add exit command

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def main():
+    
+    while True:
+
+        sys.stdout("$")
+
+        command = input("")
+
+        if command == "exit 0":
+            sys.exit(0)
+        else:
+            print(f"{command}: command not found")
+
+if __name__ == "__main__":
+    main()
