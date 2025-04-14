be73d14ae79797daff801a427eeee7c26c0e3cc6
TheSlumberingEgg <aramawad05@gmail.com>
2025-04-14 12:28:15 -0400

    add props_to_html method and test cases

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-import sys
+#! /c/Users/arama/AppData/Local/Microsoft/WindowsApps/python
 
+import sys
+import shutil
 
 def main():
     
+    builtin = ["echo", "exit"]
+
     while True:
 
         sys.stdout.write("$ ")
 
-        command = input("").split(" ")
+        user_input = input("")
+        command = user_input.split()[0]
+        args = user_input.split()[1:]
 
-        if command[0] == "echo":
-            sys.stdout.write(str.join(" ", command[1:]) + "\n")
-        elif command[0] == "exit" and command[1] == "0":
+        if command == "echo":
+            sys.stdout.write(str.join(" ", args) + "\n")
+        elif command == "type":
+            if args in builtin:
+                sys.stdout.write(f"{args} is a shell builtin\n")
+            elif path := shutil.which(args):
+                print(f"{args} is {path}")
+        elif command == "exit" and args == "0":
             sys.exit(0)
         else:
-            sys.stdout.write(f"{command[0]}: command not found")
+            sys.stdout.write(f"{command[0]}: command not found\n")
 
 if __name__ == "__main__":
     main()
